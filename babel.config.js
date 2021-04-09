a3566d53512b29077d0513b6ff77c5604679e16a
李剑坡 <18959261995@163.com>
2021-04-09 17:48:11 +0800

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@containers': './src/containers',
+          '@actions': './src/actions',
+          '@reducers': './src/reducers',
+          '@util': './src/util',
+          '@models': './src/models',
+          '@router': './src/router',
+          '@service': './src/service',
+          '@ui': './src/ui',
+          '@features': './src/features',
+          '@source': './src/source',
+          // '@images': './src/source/images',
+          '@contexts': './src/contexts',
+          '@native': './src/native',
+          '@hooks': './src/hooks',
+          '@pages': './src/pages',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+}
