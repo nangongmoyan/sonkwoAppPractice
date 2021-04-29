cc0a105169c7391cddabe78c2e43a9b00b3531fc
李剑坡 <18959261995@163.com>
2021-04-29 18:14:45 +0800

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
  * @format
  */
 import React from 'react'
-import { AppRegistry } from 'react-native'
+import { AppRegistry, Platform } from 'react-native'
 import SonkwoAppPractice from './src/main'
+import { config } from '@sonkwo/sonkwo-api'
 import { name as appName } from './app.json'
+
+// 初始化 sonkwo-api 请求配置
+config.setOs(Platform.OS)
+config.setVersion('5.9.0')
+config.setBuildConfig('debug')
+// config.setBuildConfig(isDev ? 'debug' : 'release')
 
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render')
