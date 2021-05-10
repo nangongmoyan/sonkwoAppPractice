436b518df80962435645752fcf0fba6e9e737f98
李剑坡 <18959261995@163.com>
2021-05-10 15:59:39 +0800

    first commit

---
+++ android/app/src/main/java/com/sonkwoapppractice/invokenative/OpenAppSettingsModule.java
@@ -0,0 +1,4 @@
+package com.sonkwoapppractice.invokenative;
+
+public class OpenAppSettingsModule {
+}
---
+++ ios/RCTHttpCache.m
@@ -0,0 +1,12 @@
+//
+//  RCTHttpCache.m
+//  sonkwoAppPractice
+//
+//  Created by sonkwo on 2021/5/10.
+//
+
+#import "RCTHttpCache.h"
+
+@implementation RCTHttpCache
+
+@end
